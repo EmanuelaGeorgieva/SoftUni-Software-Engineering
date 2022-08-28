e712796e1d339fa7601107e18eaa7f25ce61a125
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-08-28 13:37:57 +0300

    Create E03TimePlus15Minutes.java

---
+++ Java Basic/Java Basic Exercise/Conditional Statements Exercise/E03TimePlus15Minutes.java
@@ -0,0 +1,22 @@
+package conditionalStatementsExercise;
+
+import java.util.Scanner;
+
+public class E03TimePlus15Minutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int initHour = Integer.parseInt(scanner.nextLine());
+        int initMin = Integer.parseInt(scanner.nextLine());
+
+        int allMin = (initHour * 60) + initMin + 15;
+
+        int hour = allMin / 60;
+        int min = allMin % 60;
+        if (hour > 23){
+            hour = 0;
+        }
+
+        System.out.printf("%d:%02d" , hour , min);
+    }
+}
