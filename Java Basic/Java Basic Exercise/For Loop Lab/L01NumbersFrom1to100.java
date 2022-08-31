4ff67e861295c4ca4775d49748ec0b5e74e444b5
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-08-31 17:26:00 +0300

    Create L01NumbersFrom1to100.java

---
+++ Java Basic/Java Basic Exercise/For Loop Lab/L01NumbersFrom1to100.java
@@ -0,0 +1,11 @@
+package forLoopLab;
+
+public class L01NumbersFrom1to100 {
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 100; number++){
+            System.out.println(number);
+        }
+
+    }
+}
