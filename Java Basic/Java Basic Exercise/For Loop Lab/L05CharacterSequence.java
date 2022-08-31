f8c51c015db806019da18c473bdbb869c0b28a91
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-08-31 17:39:24 +0300

    Create L05CharacterSequence.java

---
+++ Java Basic/Java Basic Exercise/For Loop Lab/L05CharacterSequence.java
@@ -0,0 +1,15 @@
+package forLoopLab;
+
+import java.util.Scanner;
+
+public class L05CharacterSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+        for (int i = 0; i < text.length(); i++){
+            char letter = text.charAt(i);
+            System.out.println(letter);
+        }
+    }
+}
