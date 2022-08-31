913f276627d31888a8ce411ff616d9e7062daf64
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-08-31 17:30:12 +0300

    Create L03Numbers1ToNWithStep3.java

---
+++ Java Basic/Java Basic Exercise/For Loop Lab/L03Numbers1ToNWithStep3.java
@@ -0,0 +1,14 @@
+package forLoopLab;
+
+import java.util.Scanner;
+
+public class L03Numbers1ToNWithStep3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int number = 1; number <= n; number += 3){
+            System.out.println(number);
+        }
+    }
+}
