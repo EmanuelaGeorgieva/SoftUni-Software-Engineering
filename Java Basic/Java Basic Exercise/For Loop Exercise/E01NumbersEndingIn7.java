266a12bcc0ff8f0801af32ad470b6bdf00e7cfc9
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-09-01 12:58:14 +0300

    add

---
+++ Java Basic/Java Basic Exercise/For Loop Exercise/E01NumbersEndingIn7.java
@@ -0,0 +1,16 @@
+package forLoopExercise;
+
+import java.util.Scanner;
+
+public class E01NumbersEndingIn7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 7; i <= 997; i++) {
+            if (i % 10 == 7) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
