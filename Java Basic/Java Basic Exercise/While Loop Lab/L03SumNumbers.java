a9a05843ccf2cd170d411fdd3c208c5ada8c2c6b
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-09-02 11:44:17 +0300

    Create L03SumNumbers.java

---
+++ Java Basic/Java Basic Exercise/While Loop Lab/L03SumNumbers.java
@@ -0,0 +1,19 @@
+package whileLoopLab;
+
+import java.util.Scanner;
+
+public class L03SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int controlNumber = Integer.parseInt(scanner.nextLine());
+
+        int sum = 0;
+        while (sum < controlNumber){
+            int number = Integer.parseInt(scanner.nextLine());
+            sum += number;
+        }
+        System.out.println(sum);
+
+    }
+}
