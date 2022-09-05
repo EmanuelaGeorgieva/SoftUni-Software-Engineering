c88253ef3f409af7fcac5c61424b86f16923a8e2
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-09-05 18:43:03 +0300

    Create L01Clock.java

---
+++ Java Basic/Java Basic Exercise/Nested Loops Lab/L01Clock.java
@@ -0,0 +1,13 @@
+package nestedLoopsLab;
+
+public class L01Clock {
+    public static void main(String[] args) {
+        for (int hour = 0; hour <= 23 ; hour++) {
+            for (int minutes = 0; minutes <= 59 ; minutes++) {
+                System.out.println(hour + ":" + minutes);
+
+            }
+
+        }
+    }
+}
