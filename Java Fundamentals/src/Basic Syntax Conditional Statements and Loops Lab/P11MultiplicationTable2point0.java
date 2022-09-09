0afc0a10139bbe735aaeb7c9a6a0f6056d0c0321
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-09-10 00:05:09 +0300

    Create P11MultiplicationTable2point0.java

---
+++ Java Fundamentals/src/Basic Syntax Conditional Statements and Loops Lab/P11MultiplicationTable2point0.java
@@ -0,0 +1,18 @@
+package basicSyntaxConditionalStatementsAndLoopsLab;
+
+import java.util.Scanner;
+
+public class P11MultiplicationTable2point0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int times = Integer.parseInt(scanner.nextLine());
+
+        if (times > 10){
+            System.out.printf("%n%d X %d = %d",number, times, times * number);
+        }
+        for (int i = times; i <= 10 ; i++) {
+            System.out.printf("%n%d X %d = %d",number, i, i * number);
+        }
+    }
+}
