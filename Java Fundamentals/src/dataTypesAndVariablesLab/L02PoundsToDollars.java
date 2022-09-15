a83da2345c5251f523876f97f4704a618c97e7d6
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-09-15 22:01:32 +0300

    Create L02PoundsToDollars.java

---
+++ Java Fundamentals/src/dataTypesAndVariablesLab/L02PoundsToDollars.java
@@ -0,0 +1,15 @@
+package dataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class L02PoundsToDollars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double pound = Double.parseDouble(scanner.nextLine());
+        double dollars = pound * 1.36;
+
+        System.out.printf("%.3f", dollars);
+
+    }
+}
