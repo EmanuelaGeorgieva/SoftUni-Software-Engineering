957dcc9ea4a8758a2b18302a1551f17ca173d1f5
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-09-26 23:59:29 +0300

    Create L04ReverseArrayOfStrings.java

---
+++ Java Fundamentals/src/Arrays Lab/L04ReverseArrayOfStrings.java
@@ -0,0 +1,18 @@
+package arraysLab;
+
+import java.util.Scanner;
+
+public class L04ReverseArrayOfStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputArray = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < inputArray.length / 2; i++) {
+            String oldElement = inputArray[i];
+            inputArray[i] = inputArray[inputArray.length - 1 -i];
+            inputArray[inputArray.length - 1 - i] = oldElement;
+        }
+        System.out.println(String.join(" ", inputArray));
+    }
+}
