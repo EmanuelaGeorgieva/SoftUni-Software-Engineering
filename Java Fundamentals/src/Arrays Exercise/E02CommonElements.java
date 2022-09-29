224c17be32627324b03d313148097a77b1b6060f
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-09-29 11:11:26 +0300

    Create E02CommonElements.java

---
+++ Java Fundamentals/src/Arrays Exercise/E02CommonElements.java
@@ -0,0 +1,24 @@
+package arraysExercise;
+
+import java.util.Scanner;
+
+public class E02CommonElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstRow = scanner.nextLine();
+        String secondRow = scanner.nextLine();
+
+        String[] firstArray = firstRow.split(" ");
+        String[] secondArray = secondRow.split(" ");
+
+        for (String secondElement : secondArray) {
+            for (String firstElement : firstArray) {
+                if (firstElement.equals(secondElement)) {
+                    System.out.print(firstElement + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
