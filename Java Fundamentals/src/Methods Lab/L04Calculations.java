0a9807911cbca9e0c328a1989375519f47d67f78
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-10-11 18:07:07 +0300

    Create L04Calculations.java

---
+++ Java Fundamentals/src/Methods Lab/L04Calculations.java
@@ -0,0 +1,48 @@
+package methodsLab;
+
+import java.util.Scanner;
+
+public class L04Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String method = scanner.nextLine();
+        int firstNumInput = Integer.parseInt(scanner.nextLine());
+        int secondNumInput = Integer.parseInt(scanner.nextLine());
+
+        switch (method) {
+            case "add":
+                printAddNumbers(firstNumInput, secondNumInput);
+                break;
+            case "multiply":
+                printMultiplyNumbers(firstNumInput, secondNumInput);
+                break;
+            case "subtract":
+                printSubtractNumbers(firstNumInput, secondNumInput);
+                break;
+            case "divide":
+                printDivideNumbers(firstNumInput, secondNumInput);
+                break;
+        }
+    }
+
+    public static void printAddNumbers(int firstNum, int secondNum) {
+        int result = firstNum + secondNum;
+        System.out.println(result);
+    }
+
+    public static void printMultiplyNumbers(int firstNum, int secondNum) {
+        int result = firstNum * secondNum;
+        System.out.println(result);
+    }
+
+    public static void printSubtractNumbers(int firstNum, int secondNum) {
+        int result = firstNum - secondNum;
+        System.out.println(result);
+    }
+
+    public static void printDivideNumbers(int firstNum, int secondNum) {
+        int result = firstNum / secondNum;
+        System.out.println(result);
+    }
+}
