e1136d9500bcc01509a5ecdef919042b90bd55c6
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-10-17 11:40:51 +0300

    Create E02VowelsCount.java

---
+++ Java Fundamentals/src/Methods Exercise/E02VowelsCount.java
@@ -0,0 +1,24 @@
+package methodsExercise;
+
+import java.util.Scanner;
+
+public class E02VowelsCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine().toLowerCase();
+
+        int countVowels = getVowelsCount(text);
+        System.out.println(countVowels);
+    }
+
+    private static int getVowelsCount (String text) {
+        int count = 0;
+        for (char symbol : text.toCharArray()) {
+            if (symbol == 'a' || symbol == 'e' || symbol == 'i'
+                    || symbol == 'o' || symbol == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
