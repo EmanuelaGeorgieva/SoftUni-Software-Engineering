4087c178baf6f37ee0e0342d82d72a88f44fc1eb
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-10-24 20:30:35 +0300

    Create E06MiddleCharacters.java

---
+++ Java Fundamentals/src/Methods Exercise/E06MiddleCharacters.java
@@ -0,0 +1,25 @@
+package methodsExercise;
+
+import java.util.Scanner;
+
+public class E06MiddleCharacters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        printMiddleCharacters(text);
+    }
+
+    private static void printMiddleCharacters (String text) {
+        if (text.length() % 2 == 1) {
+            int indexOfMiddleCharacter = text.length() / 2;
+            System.out.println(text.charAt(indexOfMiddleCharacter));
+        }
+        else {
+            int indexFirstMiddleCharacter = text.length() / 2 - 1;
+            int indexSecondMiddleCharacter = text.length() / 2;
+
+            System.out.println("" + text.charAt(indexFirstMiddleCharacter) + text.charAt(indexSecondMiddleCharacter));
+        }
+    }
+}
