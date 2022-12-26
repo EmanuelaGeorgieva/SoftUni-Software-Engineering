bd5a4e62fd43df80ece1994d4add57c4a831a771
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2022-12-26 15:18:00 +0200

    Create L04Songs.java

---
+++ Java Fundamentals/src/Objects And Classes Lab/L04Songs.java
@@ -0,0 +1,70 @@
+package objectsAndClassesLab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class L04Songs {
+    static  class Song{
+        String typeList;
+        String name;
+        String time;
+
+        public void setTypeList(String typeList){
+            this.typeList = typeList;
+        }
+
+        public String getTypeList() {
+            return this.typeList;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setTime(String time){
+            this.time = time;
+        }
+
+        public String getTime() {
+            return this.time;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Song> listSongs = new ArrayList<>();
+        for (int i = 0; i < n ; i++) {
+            String[] inputSondArr = scanner.nextLine().split("_");
+            String inputType = inputSondArr[0];
+            String inputName = inputSondArr[1];
+            String inputTime = inputSondArr[2];
+
+            Song currentSong = new Song();
+            currentSong.setTypeList(inputType);
+            currentSong.setName(inputName);
+            currentSong.setTime(inputTime);
+
+            listSongs.add(currentSong);
+        }
+        String command = scanner.nextLine();
+        if (command.equals("all")){
+            for (Song item : listSongs){
+
+                System.out.println(item.getName());
+            }
+        }else{
+            for (Song iteam : listSongs) {
+                if (iteam.getTypeList().equals(command))
+                    System.out.println(iteam.getName());
+            }
+        }
+    }
+}
