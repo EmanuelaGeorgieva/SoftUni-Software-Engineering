61cdc7e28b9f9be6e4e5a3d5ba0761501d9c77f1
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2023-02-08 13:50:10 +0200

    Update L07CondenseArrayToNumber.java

--- Java Fundamentals/src/Arrays Lab/L07CondenseArrayToNumber.java
+++ Java Fundamentals/src/Arrays Lab/L07CondenseArrayToNumber.java
@@ -6,17 +6,20 @@ import java.util.Scanner;
 public class L07CondenseArrayToNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] inArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(e ->Integer.parseInt(e)).toArray();
 
-        while (inArr.length > 1){
-            int[] condArr = new int[inArr.length -1];
-            for (int j = 0; j < inArr.length-1; j++) {
-                condArr[j] = inArr[j] + inArr[j +1];
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        while (numbers.length > 1){
+            int[] condensed = new int[numbers.length -1];
+
+            for (int i = 0; i < numbers.length - 1; i++) {
+                condensed[i] = numbers[i] + numbers[i + 1];
             }
 
-            inArr = condArr;
-
+            numbers = condensed;
         }
-        System.out.println(inArr[0]);
+
+        System.out.println(numbers[0]);
     }
 }
