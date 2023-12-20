9387dc5e881d4fb6321abd1c0d1047b61287ab06
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2023-12-20 12:40:04 +0200

    Update 07Shopping.js

--- Javascript Basic/Conditional Statements - Exercise/07Shopping.js
+++ Javascript Basic/Conditional Statements - Exercise/07Shopping.js
@@ -5,9 +5,9 @@ function shopping(input){
     const countOfProcessors = Number(input[2]);
     const countOfRamMemory = Number(input[3]);
 
-    let totalVideoCard = countOfVideoCards * videoCard;
-    let totalProcessors = (totalVideoCard * 0.35) * countOfProcessors;
-    let totalRamMemory = (totalVideoCard * 0.1) * countOfRamMemory;
+    const totalVideoCard = countOfVideoCards * videoCard;
+    const totalProcessors = (totalVideoCard * 0.35) * countOfProcessors;
+    const totalRamMemory = (totalVideoCard * 0.1) * countOfRamMemory;
 
     let totalPrice = totalVideoCard + totalProcessors + totalRamMemory;
 
@@ -20,12 +20,11 @@ function shopping(input){
         console.log(`You have ${leftMoney.toFixed(2)} leva left!`);
     }else{
         const neededMoney = totalPrice - peterBudget;
-        console.log(`Not enough money! You need ${neededMoney.toFixed(2)} leva more`);
+        console.log(`Not enough money! You need ${neededMoney.toFixed(2)} leva more!`);
     }
 }
 
 shopping(["920.45",
 "3",
 "1",
-"1"])
-;
+"1"]);
