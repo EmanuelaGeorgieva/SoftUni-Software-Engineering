b4b38f9d3b42ab7c96e8c19064ed8ccb26e68739
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2023-12-27 13:47:50 +0200

    Create 05SmallShop.js

---
+++ Javascript Basic/Conditional Statements Advanced - Lab/05SmallShop.js
@@ -0,0 +1,69 @@
+function smallShop(input){
+    const product = input[0];
+    const town = input[1];
+    const count = input[2];
+
+    let price = 0;
+    if (town == 'Sofia') {
+        switch(product){
+            case "coffee":
+                price = 0.5;
+                break;
+            case "water":
+                price = 0.8;
+                break;
+            case "beer":
+                price = 1.2;
+                break; 
+            case "sweets":
+                price = 1.45;
+                break;
+            case "peanuts" :
+                price = 1.6;
+                break;      
+        }
+    }else if(town == 'Plovdiv'){
+        switch(product){
+            case "coffee":
+                price = 0.4;
+                break;
+            case "water":
+                price = 0.7;
+                break;
+            case "beer":
+                price = 1.15;
+                break; 
+            case "sweets":
+                price = 1.3;
+                break;
+            case "peanuts" :
+                price = 1.5;
+                break;
+        } 
+    }else if(town == 'Varna'){
+        switch(product){
+            case "coffee":
+                price = 0.45;
+                break;
+            case "water":
+                price = 0.7;
+                break;
+            case "beer":
+                price = 1.1;
+                break; 
+            case "sweets":
+                price = 1.35;
+                break;
+            case "peanuts" :
+                price = 1.55;
+                break; 
+        }
+    }
+
+    const total = count * price;
+    console.log(total)
+}
+
+smallShop(["coffee",
+"Varna",
+"2"]);
