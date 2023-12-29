5b40a156326429572be3b0646eab8e4ac0f11849
Emanuela Georgieva <106863506+EmanuelaGeorgieva@users.noreply.github.com>
2023-12-29 20:39:22 +0200

    Create 07WorkingHours.js

---
+++ Javascript Basic/Conditional Statements Advanced - Lab/07WorkingHours.js
@@ -0,0 +1,13 @@
+function workingHours(input){
+    const time = Number(input[0]);
+    const day = input[1];
+
+    if(time >= 10 && time <= 18 && day != 'Sunday'){
+        console.log('open');
+    }else{
+        console.log('closed');
+    }
+}
+
+workingHours(["19",
+"Friday"]);
